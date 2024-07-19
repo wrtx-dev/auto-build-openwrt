6db990ef898aadf959334635e270736f9caf3bf5
wrtx-dev <wrtx.dev@outlook.com>
2024-07-19 19:49:13 +0800

    set commit id manual

--- clone_openwrt_by_commit_id.sh
+++ clone_openwrt_by_commit_id.sh
@@ -2,11 +2,19 @@
 
 [ -d openwrt ] && rm -rvf openwrt
 
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 commit_id"
+    exit 1
+fi
+
+commit_id=${1}
+
 mkdir openwrt
 
 cd openwrt
 
-git init && git remote add origin https://git.openwrt.org/openwrt/openwrt.git && git fetch origin 715634e6d1443eacdcb84b04d1028c1564b08dbf && git reset --hard FETCH_HEAD || exit 1
+git init && git remote add origin https://git.openwrt.org/openwrt/openwrt.git && git fetch origin ${commit_id} && git reset --hard FETCH_HEAD || exit 1
 
 
 echo -e "src-git passwalldep https://github.com/xiaorouji/openwrt-passwall-packages.git \nsrc-git passwall https://github.com/xiaorouji/openwrt-passwall2.git" >> feeds.conf.default || exit 1
