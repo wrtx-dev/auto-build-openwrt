275a5131ff189fb1e5dccef90e29f54daf2ee320
wrtx-dev <wrtx.dev@outlook.com>
2024-07-21 21:41:04 +0800

    add smartdns

--- x.sh
+++ x.sh
@@ -9,8 +9,8 @@ tmate -S /tmp/tmate.sock wait tmate-ready
 echo ________________________________________________________________________________
 echo
 echo To connect to this session copy-n-paste the following into a terminal:
-tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
-tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
+WEBSSH=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')
+SSH=$(tmate -S /tmp/tmate.sock display -p '#{tmate_web}')
 
 timeout=$((150*60))
 while [ -S /tmp/tmate.sock ]; do
@@ -20,5 +20,7 @@ while [ -S /tmp/tmate.sock ]; do
         echo Waiting on tmate connection timed out!
         exit 1
     fi
+    echo ${WEBSSH}
+    echo ${SSH}
 done
 true
